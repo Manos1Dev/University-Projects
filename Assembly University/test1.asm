4d355308e945090ea0d2d1fb22239a692055661b
Manos1 <50167081+Manos121@users.noreply.github.com>
2021-05-19 17:19:18 +0300

    tested variables + arrays + strings + dup + equ-textequ (errors)

--- Assembly University/test1.asm
+++ Assembly University/test1.asm
@@ -1,15 +1,22 @@
 TITLE geia
 INCLUDE Irvine32.inc
 
+integer EQU <"Press any key to continue...">
+
 .data
-    arr word 1, 2, 3, 4, 5, 6
-    test word 0
+    manos byte "malaka", 0 
+    constant DWORD 74h
+    msg TEXTEQU %integer
+    ;test1 SWORD 
 
 .code
 
 main PROC 
-    mov test,[arr + 2]
-    call writeInt
+    mov edx, offset manos
+    ;add eax, 2
+    call WriteString
+    mov edx, offset msg
+    call WriteInt
     exit
 main ENDP
 END main
