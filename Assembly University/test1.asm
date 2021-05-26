0395ed67aa9049255cd1228f1d65cf0a1345d23e
Manos1 <50167081+Manos121@users.noreply.github.com>
2021-05-26 12:23:02 +0300

    enan register ton kanw FFFFFFFF (px eax)

--- Assembly University/test1.asm
+++ Assembly University/test1.asm
@@ -1,22 +1,13 @@
 TITLE geia
 INCLUDE Irvine32.inc
 
-integer EQU <"Press any key to continue...">
-
 .data
-    manos byte "malaka", 0 
-    constant DWORD 74h
-    msg TEXTEQU %integer
-    ;test1 SWORD 
+     
 
 .code
 
-main PROC 
-    mov edx, offset manos
-    ;add eax, 2
-    call WriteString
-    mov edx, offset msg
-    call WriteInt
+main PROC
+    mov eax, 0FFFFFFFFh
     exit
 main ENDP
 END main
